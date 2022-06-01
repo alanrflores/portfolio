3ab85848613b4e9914d03844688491a988f1b9b1
alanrflores <alan.flores640@gmail.com>
2022-06-01 15:27:19 -0300

    nuevos cambios

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,35 +1,43 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-    <div className='footer-info'>
-    <h1>Your name</h1>
-    <p>Argentina, Buenos Aires</p>
-    <div className='footer-contact'>
-    <h3>Contacto</h3>
-    </div>
-    <div className='footer-sns'>
-    <div className='design-by'>
-    Diseñado por Alan
-    </div>
-     <div className='sns-link'>
-     <a href="https://www.linkedin.com/in/alan-flores-184406171/" target="_blank" rel="noreferrer"> 
-        <i className='fab fa-linkedin linkedin'></i>
-     </a>
-     <a href="https://www.instagram.com/floresalanr/" target="_blank" rel="noreferrer" >
-        <i className='fab fa-instagram instagram'></i>
-     </a>
-     <a href="https://www.instagram.com/floresalanr/" target="_blank" rel="noreferrer" >
-        <i className='fab fa-facebook facebook'></i>
-     </a>
-
-     </div>
-    </div>
-    </div>
+    <footer className="footer">
+      <div className="footer-info">
+        <p>Argentina, Buenos Aires</p>
+      </div>
+      <div className="footer-contact">
+        <h3>Contacto</h3>
+      </div>
+      <div className="footer-sns">
+        <div className="design-by">Diseñado por Alan</div>
+        <div className="sns-link">
+          <a
+            href="https://www.linkedin.com/in/alan-flores-184406171/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin linkedin"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/floresalanr/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-instagram instagram"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/floresalanr/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-facebook facebook"></i>
+          </a>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({isScrolling}) => {
   return (
     <nav className={`navbar ${isScrolling > 20 ? "scrolling" : null}`}>
     <div className="navbar-logo" onClick={toTheTop}>
-      Alan 
+      Inicio
     </div>
   </nav>
   )
