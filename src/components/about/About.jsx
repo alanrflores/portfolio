65284c611353fbe120df269bdbb245533662e42b
alanrflores <alan.flores640@gmail.com>
2023-04-04 15:51:52 -0300

    update changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,38 +1,46 @@
 import React from "react";
-import style from './about.module.css'
+import style from "./about.module.css";
 
- 
 const About = () => {
-  const image = "alan3.jpg"
+  const image = "alan3.jpg";
 
   return (
-   <div className={style.divBody}>    
-    <div className={style.card}>
-      <div className={style.lines}></div>
-      <div className={style.imgBox}>
-        <img src={image} alt="imagen-perfil" />
-      </div>
-      <div className={style.content}>
-        <div className={style.details}> 
-           <h2>Full Stack Developer</h2>
-           <hr />
-           <div className={style.data}>
-            <h4>¡Hola!<br/>
-            <span> 
-            Comencé con la tecnología desde chico, ya que en la secundaria me enseñaban Ciencias de la Informática, 
-            decidí seguir en la facultad pero en ese momento estaba muy aislado ya que me sentía solo, sin motivación,
-            me dedique a otra cosa! <br /> 
-            Hoy siento que vuelvo realmente a hacer lo que me gusta.<br/>
-            Graduado de la carrera Full Stack Developer
-            donde he redescubierto mi pasión por eso, y estoy muy emocionado de seguir por este camino...<br/>
-            <br/>
-            Gracias por llegar hasta aca!
-            </span>
-            </h4>
-           </div>
+    <div className={style.divBody}>
+      <div className={style.card}>
+        <div className={style.lines}></div>
+        <div className={style.imgBox}>
+          <img src={image} alt="imagen-perfil" />
+        </div>
+        <div className={style.content}>
+          <div className={style.details}>
+            <h2>Full Stack Developer</h2>
+            <hr />
+            <div className={style.data}>
+              <h4>
+                ¡Hola!
+                <br />
+                <span>
+                  Mi objetivo es brindar, adquirir, compartir y desarrollar mis experiencias
+                  para un mejor desempeño profesional. <br /> 
+                  Obtener un empleo en el
+                  que mi pensamiento innovador se pueda usar en conjunto para
+                  ayudar a la organización a mejorar y acrecentar su visión y
+                  misión.  <br />
+                  Aplicar mis talentos y ofrecer los conocimientos que
+                  poseo a nivel personal y profesional a la compañía que me
+                  brinde la oportunidad, trabajando arduamente con integridad,
+                  honestidad, compromiso, excelencia, y profesionalismo;
+                  participando activa y efectivamente en las actividades que
+                  promueven el éxito continuo.
+                  <br />
+                  <br />
+                  Gracias por llegar hasta aca!
+                </span>
+              </h4>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -7,7 +7,7 @@ const Cover = () => {
     <div className={style.containerCover}>
      <video className={style.video} src={coverVideo} autoPlay loop muted></video>
      <h1>Alan Rodrigo Flores</h1>
-     <p data-text="Full Stack Developer | Frontend Developer"></p>
+     <p data-text="Full Stack Developer"></p>
     </div>
   )
 }
